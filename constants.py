f5571c74e3cbe5953a332fffc63b46cbff7e42d5
Ubuntu <ubuntu@ip-172-31-12-63.ec2.internal>
2021-09-17 17:33:07 +0000

    push

---
+++ constants.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env python3
+
+# Created by Trent Hodgins
+# Created on 09/17/2021
+# This module contains constants for the guessing game program
+
+CORRECT_NUMBER = 5
---
+++ guessing_game.py
@@ -0,0 +1,30 @@
+# !/user/bin/env python3
+
+# Created by Trent Hodgins
+# Created on 09/17/20
+# This is a guessing game program
+# The user enters in a number between 1 and 9
+
+import constants
+
+
+def main():
+    # this function checks to see if the user guessedthe orrect number(5)
+
+    # input
+    guessed_number = int(input("Guess a number between 0 and 9 (integer): "))
+    print("")
+
+    # process and output
+    if constants.CORRECT_NUMBER == guessed_number:
+        print("You Guessed Correctly")
+        print("")
+        print("Done.")
+    else:
+        print("You Guessed Wrong!")
+        print("")
+        print("Done.")
+
+
+if __name__ == "__main__":
+    main()
